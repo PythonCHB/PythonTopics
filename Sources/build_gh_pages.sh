fc17ba7b6a5d59fee842fe1d0ebf10a5d1e5561e
Chris Barker <PythonCHB@gmail.com>
2015-12-13 13:14:42 -0800

    made gh-pages build script more robust

--- Sources/build_gh_pages.sh
+++ Sources/build_gh_pages.sh
@@ -12,5 +12,6 @@ cp -R build/html/ ../../PythonTopics_gh_pages/
 cd ../../PythonTopics_gh_pages
 git checkout gh-pages
 git add * # in case there are new files added
-git commit -a
+git commit -a -m "updating published version"
+git pull -s ours --no-edit
 git push
